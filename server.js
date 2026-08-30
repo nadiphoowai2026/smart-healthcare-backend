e2b1a43ce26b76a98cd5a6a6fc984f4f2378a95a
nadiphoowai2026 <nadiphoowai9@gmail.com>
2026-08-30 20:35:44 +0630

    Update server.js

--- server.js
+++ server.js
@@ -552,7 +552,7 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, "0.0.0.0", () => {
 
     console.log(
-        "Server running on port " + PORT
+        `Server running on port${PORT}`
     );
 
 });
